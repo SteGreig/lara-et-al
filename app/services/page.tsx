3f2ebbadcd02adad23704eeebe2f35e8d97095ee
Stephen Greig <steve.greig1@gmail.com>
2024-09-26 22:59:47 +0100

    Services

---
+++ app/services/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Services',
+  description:
+    'services page',
+};
+
+const pageServices = () => {
+  return (
+    <div>pageServices</div>
+  )
+}
+
+export default pageServices
