e4ba92ddd7113cdef0203a25e7968037beb88a46
Stephen Greig <steve.greig1@gmail.com>
2024-09-26 23:06:56 +0100

    Projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Projects',
+  description:
+    'Projects page',
+};
+
+const pageProjects = () => {
+  return (
+    <div>pageProjects</div>
+  )
+}
+
+export default pageProjects
