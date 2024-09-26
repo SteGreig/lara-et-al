1b988f79372c88d05728db86b8ba60a304b8a0bd
Stephen Greig <steve.greig1@gmail.com>
2024-09-26 23:07:36 +0100

    Contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Contact',
+  description:
+    'Contact page',
+};
+
+const pageContact = () => {
+  return (
+    <div>pageContact</div>
+  )
+}
+
+export default pageContact
