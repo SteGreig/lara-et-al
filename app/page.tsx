fd10d7e292dedec5ac7c9d782951b059e746ddad
Stephen Greig <steve.greig1@gmail.com>
2024-09-28 23:09:29 +0100

    Home content styling

--- app/page.tsx
+++ app/page.tsx
@@ -28,8 +28,8 @@ export default async function Home() {
       />
 
       <main className="container ~mt-16/28">
-        <h1 className="large-headline 2xl:w-11/12">{home.contentHeadline}</h1>
-        <div className="flex flex-col md:flex-row">
+        <h1 className="large-headline 2xl:w-11/12 ~mb-4/20">{home.contentHeadline}</h1>
+        <div className="flex flex-col md:flex-row ~gap-6/20">
           {page.contentImage && (
             <aside className="w-full md:w-5/12">
               <Image
