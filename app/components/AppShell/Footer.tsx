ed17e0584f80984a8258fca782c3389af3d1621d
Stephen Greig <steve.greig1@gmail.com>
2024-09-29 11:30:32 +0100

    Footer mobile fixes

--- app/components/AppShell/Footer.tsx
+++ app/components/AppShell/Footer.tsx
@@ -10,11 +10,11 @@ export default async function Footer() {
 
   return (
     <footer className="footer mt-24 xl:mt-36">
-      <div className="px-2 lg:px-4 2xl:px-6 -mb-3">
+      <div className="px-2 lg:px-4 2xl:px-6 ~-mb-1/3">
         <LogoTextureSvg />
       </div>
 
-      <div className="bg-noise-30 bg-primary text-cream container py-7 grid grid-cols-12 uppercase tracking-widest text-sm">
+      <div className="bg-noise-30 bg-primary text-cream container py-7 flex flex-col gap-4 lg:grid md:grid-cols-12 uppercase tracking-widest text-sm">
         <p className="col-start-1">&copy; {currentYear}</p>
         {site.socials && (
           <ul className="col-start-3 flex gap-4">
@@ -25,7 +25,7 @@ export default async function Footer() {
             ))}
           </ul>
         )}
-        <p className="-col-end-1 justify-self-end whitespace-nowrap">
+        <p className="-col-end-1 justify-self-end lg:whitespace-nowrap">
           {site.address}
         </p>
       </div>
