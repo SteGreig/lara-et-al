40e701c2524ffd1d66bf70f8edf2815a5631b1d6
Stephen Greig <steve.greig1@gmail.com>
2024-10-25 00:35:06 +0100

    Hero banner tweaks

--- app/components/HeroBanner/HeroBanner.tsx
+++ app/components/HeroBanner/HeroBanner.tsx
@@ -3,7 +3,7 @@
 const HeroBanner = ({headline}: {headline: string}) => {
   return (
     <div className="bg-primary bg-noise-30 ~pt-20/40 text-cream">
-      <h1 className="text-[13vw] font-bold -tracking-wider leading-[1.2] container">{headline}</h1>
+      <h1 className="text-[12vw] font-bold -tracking-wider leading-[.9] container">{headline}</h1>
     </div>
   )
 }
